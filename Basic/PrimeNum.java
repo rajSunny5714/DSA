4bf0a66a60ecba1f3f98f9cd5e0ace118376ba10
sunnyraj <mrsj5714@gmail.com>
2026-01-22 17:56:43 +0530

    added files

--- Basic/PrimeNum.java
+++ Basic/PrimeNum.java
@@ -4,12 +4,12 @@ public class PrimeNum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
-        if (n <= 1) {
+        if(n <= 1) {
             System.out.println("not a prime number");
             return;
         }
         boolean isPrime = true;
-        for (int i=2; i<=n/2; i++) {
+        for(int i=2; i<=n/2; i++) {
             if (n%i==0) {
                 isPrime=false;
                 break;
