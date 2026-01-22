9f18c69a8d241cf8ab3a8b8eb5e7a56758528b5c
sunnyraj <mrsj5714@gmail.com>
2026-01-22 17:57:18 +0530

    added files

--- Basic/PrimeNum.java
+++ Basic/PrimeNum.java
@@ -10,7 +10,7 @@ public class PrimeNum {
         }
         boolean isPrime = true;
         for(int i=2; i<=n/2; i++) {
-            if (n%i==0) {
+            if(n%i==0) {
                 isPrime=false;
                 break;
             }
