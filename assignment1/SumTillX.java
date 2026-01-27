93643af6420c3dae1350ab2b2eb5fdf02996466a
sunnyraj <mrsj5714@gmail.com>
2026-01-27 17:26:13 +0530

    added

---
+++ assignment1/SumTillX.java
@@ -0,0 +1,19 @@
+package assignment1;
+import java.util.Scanner;
+
+public class SumTillX {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        while(true) {
+            System.out.print("Enter a number (or x to stop): ");
+            String input = sc.next();
+            if(input.equalsIgnoreCase("x")) {
+                break;
+            }
+            int num = Integer.parseInt(input);
+            sum += num;
+        }
+        System.out.println("Sum of all numbers = "+sum);
+    }
+}
