a2b25fc88297f56950e4398b3c33c8dce0c0dd4f
sunnyraj <mrsj5714@gmail.com>
2026-01-28 16:31:15 +0530

    files added

---
+++ assignment2/GreetingMessage.java
@@ -0,0 +1,11 @@
+package assignment2;
+
+import java.util.Scanner;
+
+public class GreetingMessage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println("Hello, "+str+"\nWelcome in java tutorial!");
+    }
+}
---
+++ assignment2/SimpleInterest.java
@@ -0,0 +1,15 @@
+package assignment2;
+
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of principle, rate and time: ");
+        float p=sc.nextFloat();
+        float r=sc.nextFloat();
+        float t=sc.nextFloat();
+        float si=(p*r*t)/100;
+        System.out.println("Simple Interest: "+si);
+    }
+}
