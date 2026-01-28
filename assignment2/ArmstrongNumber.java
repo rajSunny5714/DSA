e07b866112e695ffeae8114414db6630b8f947df
sunnyraj <mrsj5714@gmail.com>
2026-01-28 17:07:17 +0530

    files added

---
+++ assignment2/ArmstrongNumber.java
@@ -0,0 +1,29 @@
+package assignment2;
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static boolean isArmstrong(int num) {
+        int original = num;
+        int sum = 0;
+        int digits = String.valueOf(num).length();
+        while(num>0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, digits);
+            num /= 10;
+        }
+        return sum == original;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter starting number: ");
+        int start = sc.nextInt();
+        System.out.print("Enter ending number: ");
+        int end = sc.nextInt();
+        System.out.println("Armstrong numbers between " +start + " and " + end + ":");
+        for(int i=start; i<=end; i++) {
+            if(isArmstrong(i)) {
+                System.out.print(i +" ");
+            }
+        }
+    }
+}
