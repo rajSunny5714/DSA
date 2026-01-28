487ca065ee88e5de89bfd51e017f08145f82f64b
sunnyraj <mrsj5714@gmail.com>
2026-01-28 16:51:34 +0530

    files added

---
+++ assignment2/RupeesToDollar.java
@@ -0,0 +1,13 @@
+package assignment2;
+import java.util.Scanner;
+
+public class RupeesToDollar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double exchangeRate = 0.012;
+        System.out.print("Enter amount in Rupees: ");
+        double rupees = sc.nextDouble();
+        double dollars = rupees * exchangeRate;
+        System.out.println("Amount in Dollars: $" +dollars);
+    }
+}
