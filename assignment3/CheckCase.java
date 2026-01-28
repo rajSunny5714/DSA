fe227ee5773df38941355b993f062dfd63edc216
sunnyraj <mrsj5714@gmail.com>
2026-01-28 18:07:30 +0530

    files added

---
+++ assignment3/CheckCase.java
@@ -0,0 +1,18 @@
+package assignment3;
+
+import java.util.Scanner;
+
+public class CheckCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().trim().charAt(0);
+
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println("Lowercase");
+        } else if (ch >= 'A' && ch <= 'Z') {
+            System.out.println("Uppercase");
+        } else {
+            System.out.println("Another character");
+        }
+    }
+}
