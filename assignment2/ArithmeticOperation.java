9395b7378b7b78c44fe001205a0c50d5712758c1
sunnyraj <mrsj5714@gmail.com>
2026-01-28 16:43:02 +0530

    files added

---
+++ assignment2/ArithmeticOperation.java
@@ -0,0 +1,17 @@
+package assignment2;
+
+import java.util.Scanner;
+
+public class ArithmeticOperation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum=a+b;
+        int sub=a-b;
+        int multi=a*b;
+        int div=a/b;
+        int rem=a%b;
+        System.out.println("Add: "+sum+"\nSub: "+sub+"\nMultiply: "+multi+"\nDivision: "+div+"\nRemainder: "+rem);
+    }
+}
---
+++ assignment2/LargerNumber.java
@@ -0,0 +1,18 @@
+package assignment2;
+
+import java.util.Scanner;
+
+public class LargerNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a>b)
+            System.out.println("a is greater");
+        else if(a==b)
+            System.out.println("both number are equal");
+        else{
+            System.out.println("b is greater");
+        }
+    }
+}
