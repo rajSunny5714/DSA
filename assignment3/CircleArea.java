cf36be7652accb642331bdbec1e0cca0a2c1ba07
sunnyraj <mrsj5714@gmail.com>
2026-01-28 20:03:25 +0530

    find circle area

---
+++ assignment3/CircleArea.java
@@ -0,0 +1,12 @@
+package assignment3;
+
+import java.util.Scanner;
+
+public class CircleArea {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float r = sc.nextFloat();
+        float area = (22.0f / 7) * r * r;
+        System.out.println("Area of circle: " + area);
+    }
+}
