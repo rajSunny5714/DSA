15aa8d02141d4bf14ba895fa566d1ed17bf78514
sunnyraj <mrsj5714@gmail.com>
2026-02-21 12:49:12 +0530

    swap two numbers by array

---
+++ functions/Swap.java
@@ -0,0 +1,21 @@
+package functions;
+
+import java.util.Scanner;
+
+public class Swap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int[] result = swap(x, y);
+        x = result[0];
+        y = result[1];
+        System.out.println(x+" "+y);
+    }
+    private static int[] swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+        return new int[]{a, b};
+    }
+}
