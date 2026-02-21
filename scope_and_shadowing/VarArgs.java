fa329354e89eae157d29a9247bb659a45c21552d
sunnyraj <mrsj5714@gmail.com>
2026-02-21 13:28:42 +0530

    multiple arguments taking

---
+++ scope_and_shadowing/VarArgs.java
@@ -0,0 +1,26 @@
+package scope_and_shadowing;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class VarArgs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        varArgs(2,3,66,878,543,334);
+        varArgs0();
+        varArgs1("Sunny", "Raj", "Jha");
+        varArgs2(2,5, "Sunny", "Raj", "Jha");
+    }
+    private static void varArgs(int ...v){
+        System.out.println(Arrays.toString(v));
+    }
+    private static void varArgs0(String ...v) {
+        System.out.println(Arrays.toString(v));
+    }
+    private static void varArgs1(String ...v) {
+        System.out.println(Arrays.toString(v));
+    }
+    private static void varArgs2(int a, int b, String ...v) {
+        System.out.println(a+", "+b+", "+Arrays.toString(v));
+    }
+}
