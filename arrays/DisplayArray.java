78eeb7a15ceb16402c72bffe9d86c90ea386e012
sunnyraj <mrsj5714@gmail.com>
2026-02-22 17:33:26 +0530

    elements display

---
+++ arrays/DisplayArray.java
@@ -0,0 +1,20 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class DisplayArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements of array:");
+        for(int i=0; i<n; i++) {
+            arr[i]= sc.nextInt();
+        }
+        System.out.println("Elements of Array:");
+        for(int i=0; i<n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
