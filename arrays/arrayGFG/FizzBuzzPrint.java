7b00f37174744c10474e99105da1173f6d01b7f9
sunnyraj <mrsj5714@gmail.com>
2026-05-02 19:32:41 +0530

    fizzbuzz file added

---
+++ arrays/arrayGFG/FizzBuzzPrint.java
@@ -0,0 +1,19 @@
+package arrays.arrayGFG;
+
+import java.util.Scanner;
+
+public class FizzBuzzPrint {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%3==0 && n%5!=0) {
+            System.out.println("Fizz");
+        }
+        else if(n%3!=0 && n%5==0) {
+            System.out.println("Buzz");
+        }
+        else{
+            System.out.println("FizzBuzz");
+        }
+    }
+}
