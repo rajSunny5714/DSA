bf897a41084de08035e195d3665a0264d4e9e005
sunnyraj <mrsj5714@gmail.com>
2026-06-11 19:58:15 +0530

    set matrix zero file added

---
+++ leetcodeDSA/SetMatrixZero.java
@@ -0,0 +1,53 @@
+package leetcodeDSA;
+
+import java.util.Scanner;
+
+public class SetMatrixZero {
+
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[] rows = new int[n];
+        int[] cols = new int[m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = 1;
+                    cols[j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (rows[i] == 1 || cols[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] matrix = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        SetMatrixZero obj = new SetMatrixZero();
+        obj.setZeroes(matrix);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j]);
+                if (j < m - 1) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
