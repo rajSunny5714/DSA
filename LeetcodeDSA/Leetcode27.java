a41d229e2c19802cf8de3134fe89b1f19cbd4310
sunnyraj <mrsj5714@gmail.com>
2026-09-11 10:05:51 +0530

    leetcode problems

---
+++ LeetcodeDSA/Leetcode27.java
@@ -0,0 +1,29 @@
+package LeetcodeDSA;
+
+import java.util.Scanner;
+
+public class Leetcode27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array:  ");
+        int s = sc.nextInt();
+        int[] arr = new int[s];
+        System.out.println("Enter elements of the array:");
+        for(int i=0; i<s; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter the target value: ");
+        int t = sc.nextInt();
+        System.out.println("Output: "+removeElement(arr, t));
+    }
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+        for(int i=0; i<nums.length; i++) {
+            if (nums[i]!=val) {
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
