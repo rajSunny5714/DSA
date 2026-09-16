de0d1092060ef618896b27fabf31f130fc3704be
sunnyraj <mrsj5714@gmail.com>
2026-09-16 22:52:12 +0530

    check binary string code file updated

--- arrays/arrayGFG/CheckBinaryString.java
+++ arrays/arrayGFG/CheckBinaryString.java
@@ -3,6 +3,12 @@ package arrays.arrayGFG;
 import java.util.Scanner;
 
 public class CheckBinaryString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any string:");
+        String s = sc.nextLine();
+        System.out.println(isBinary(s));
+    }
     public static boolean isBinary(String s) {
         for(int i=0; i<s.length(); i++) {
             if(s.charAt(i) != '0' && s.charAt(i) != '1') {
@@ -10,11 +16,5 @@ public class CheckBinaryString {
             }
         }
         return true;
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter any string:");
-        String s = sc.nextLine();
-        System.out.println(isBinary(s));
     }
 }
